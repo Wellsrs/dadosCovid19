41d02b1f264760c08307bb83a8b7b5f5b4adc87f
Wellsrs <132627424+Wellsrs@users.noreply.github.com>
2025-04-09 09:42:52 -0300

    Update covid19.py

--- covid19.py
+++ covid19.py
@@ -1,29 +1,31 @@
 import pandas as pd
-from plotly.express import *
-plotly.express as px
+import plotly.express as px
 import streamlit as st
 
-#streamlit run codigoBase.py
 
-df = pd.read.csv("https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv")
+#LENDO O DATASET
+df = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv')
 
-df = df.rename(columns = {'newDeaths' : 'Novos Óbitos', 'newCases' : 'Novos Casos', 'deaths_per_100k_inhabitants' : 'Óbitos por 100 mil habitantes', 'totalCases_per_100k_inhabitants' : 'Casos por 100 mil habitantes'})
+#MELHORANDO O NOME DAS COLUNAS DA TABELA
+df = df.rename(columns={'newDeaths': 'Novos óbitos','newCases': 'Novos casos','deaths_per_100k_inhabitants': 'Óbitos por 100 mil habitantes','totalCases_per_100k_inhabitants':'Casos por 100 mil habitantes'})
 
+#SELECÃO DO ESTADO
 estados = list(df['state'].unique())
-state = st.sidebar.selectbox('Selecione o estado', estados)
+state = st.sidebar.selectbox('Escolha o estado', estados)
 
-colunas = ['Novos Óbitos','Novos Casos','Óbitos por 100 mil habitantes', 'Casos por 100 mil habitantes']
-column = st.sidebar.selectbox('Qual tipo de informação ?', colunas)
+#SELEÇÃO DA COLUNA
+colunas = ['Novos óbitos','Novos casos','Óbitos por 100 mil habitantes','Casos por 100 mil habitantes']
+column = st.sidebar.selectbox('Qual tipo de informação?', colunas)
 
-df = df[df['state'] == state] #seleção de linhas que pertencem ao estado
+#SELEÇÃO DAS LINHAS QUE PERTECEM AO ESTADO 
+df = df[df['state'] == state]
 
 fig = px.line(df, x="date", y=column, title=column + ' - ' + state)
-fig.update_layout(xaxis_title='Data', yaxis_title=column.upper(), title = {'x':0.5})
+fig.update_layout( xaxis_title='Data', yaxis_title=column.upper(), title = {'x':0.5})
 
-st.title('DADOS COVID19 BRASIL')
-st.write('Nessa aplicação, você terá a opção de escolher o estado e o tipo de informação para mostrar o gráfico, utilize o menu lateral para alterar os dados')
 
+st.title('DADOS COVID - BRASIL')
+st.write('Nessa aplicação, você terá a opção de escolher o estado e o tipo de informação para mostrar o gráfico. Utilize o menu lateral para alterar a mostragem.')
 st.plotly_chart(fig, use_container_width=True)
 
-st.caption('Os dados foram obtidos a partir do site https://github.com/wcota/covid19br')
-
+st.caption('Os dados foram obtidos através do site: https://github.com/wcota/covid19br')
